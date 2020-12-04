077a3b69b2b3d6014fe5d2c40062642bfcd1e3c7
Julian <julianrosenthal@yahoo.com>
2020-12-04 10:38:46 -0800

    working

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
-# require 'bundler'
-# Bundler.require
-
+require 'bundler'
+Bundler.require
+require 'pry'
+require 'awesome_print'
+require 'httparty'
 require_relative '../lib/stock_lookup/version'
 require_relative '../lib/models/stock'
 require_relative '../lib/services/api'
